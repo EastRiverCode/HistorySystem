b8d0e6960a55fce32c676c4a02140c4644957d83
estriver <yh1211dj@gmail.com>
2021-11-07 21:04:17 +0900

    second

--- java.js
+++ java.js
@@ -15,19 +15,26 @@ function maxLengthCheck(self){
     self.value = self.value.slice(0, self.maxLength);
   }
 }
-function historyLoad(){
-    var input = document.createElement("input");
-    input.type = "file";
-    input.accept = "text/plain"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
-    input.onchange = function (event) {
-        processFile(event.target.files[0]);
-    };
-    input.click();
-}
-function processFile(file) {
-    var reader = new FileReader();
-    reader.onload = function () {
-        historyWindow.innerText = reader.result;
-    };
-    reader.readAsText(file, /* optional */ "euc-kr");
-}
+
+jQuery.get('history.txt', function(data) {
+    document.getElementById("historyWindow").innerText = data;
+});
+
+
+
+// function historyLoad(){
+//     var input = document.createElement("input");
+//     input.type = "file";
+//     input.accept = "text/plain"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
+//     input.onchange = function (event) {
+//         processFile(event.target.files[0]);
+//     };
+//     input.click();
+// }
+// function processFile(file) {
+//     var reader = new FileReader();
+//     reader.onload = function () {
+//         historyWindow.innerText = reader.result;
+//     };
+//     reader.readAsText(file, /* optional */ "euc-kr");
+// }
