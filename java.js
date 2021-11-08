759338cc8fd14631a0935d0b4ca25848b6944a11
Jun_Company <yh1211dj@gmail.com>
2021-11-08 18:55:55 +0900

    10th_co

--- java.js
+++ java.js
@@ -4,15 +4,17 @@ function SearchingFunc(){
   var shuttleNumber = document.getElementsByClassName("number")[0];
   var shuttle = shuttleType.value + "-" + shuttleNumber.value;
   var shutDisp = document.getElementById("shuttleDisp");
+  var conf
 
   if(shutDisp.innerText == shuttle){
-    alert("이미 대차번호가 "+shuttle+" 입니다.");
+    alert("이미 선택된 대차입니다. "+shuttle);
   }
   else{
     shutDisp.innerText = shuttle;
     alert("대차번호 "+shuttle+" 선택되었습니다.");
   }
 }
+
 function maxLengthCheck(self){
   if (self.value.length > self.maxLength){
     //object.maxLength : 매게변수 오브젝트의 maxlength 속성 값입니다.
